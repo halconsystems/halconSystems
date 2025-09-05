757148724ff4ea8eb33aa8d05505435993f5c6af
jahangirahmed77 <jangeer9182@gmail.com>
2025-09-05 20:15:25 +0500

    v1.0

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,3 +1,5 @@
+// assets.js
+
 import logo from './logo.svg'
 import product from './product.svg'
 import bigData from './bigData.svg'
@@ -9,33 +11,13 @@ import yellowArrow from './arrow_icon.svg'
 import emailus from './emailus.svg'
 import callus from './call.svg'
 import visit from './visit.svg'
-import letsdiscuss from './letsdiscuss.svg'
+import letsDiscuss from './letsDiscuss.svg'
 import case1 from './case1.svg'
 import case2 from './case2.svg'
 import case3 from './case3.svg'
-import formimg from './formimg.png'
+import formimg from './formimg.svg'
 import buttonArrow from './buttonArrow.svg'
 import contactImage from './contactimg.svg'
-import Ai from "./staff/Ai.svg";
-import Angular from "./staff/angular.svg";
-import ApplicationSecurity from "./staff/as.svg";
-import BusinessAnalysis from "./staff/businessAnalysis.svg";
-import DataBaseIcon from "./staff/dataBaseIcon.svg";
-import DataScience from "./staff/dataScience.svg";
-import DevOps from "./staff/devOps.svg";
-import FrontEnd from "./staff/frontEnd.svg";
-import FullStack from "./staff/fullStack.svg";
-import Go from "./staff/go.svg";
-import Ise from "./staff/ise.svg";
-import Php from "./staff/PHP.svg";
-import ProductDesigner from "./staff/productDesigner.svg";
-import ProjectManager from "./staff/projectManager.svg";
-import QaEngineer from "./staff/qaEngineer.svg";
-import SolutionArchitect from "./staff/solutionArchitect.svg";
-import TeachLead from "./staff/teachLead.svg";
-import Ui from "./staff/ui.svg";
-import Wordpress from "./staff/wordpress.svg";
-import Appdev from "./staff/phone.svg";
 import innovation from './innovation.svg'
 import reliability from './reliability.svg'
 import collaboration from './collaboration.svg'
@@ -44,9 +26,29 @@ import environment from './environment.svg'
 import flexible from './flexible.svg'
 import recognition from './recognition.svg'
 
+// Staff imports
+import Ai from "./Staff/Ai.svg";
+import Angular from "./Staff/angular.svg";
+import ApplicationSecurity from "./Staff/as.svg";
+import BusinessAnalysis from "./Staff/businessAnalysis.svg";
+import DataBaseIcon from "./Staff/dataBaseIcon.svg";
+import DataScience from "./Staff/dataScience.svg";
+import DevOps from "./Staff/devOps.svg";
+import FrontEnd from "./Staff/frontEnd.svg";
+import FullStack from "./Staff/fullStack.svg";
+import Go from "./Staff/go.svg";
+import Ise from "./Staff/ise.svg";
+import Php from "./Staff/PHP.svg";
+import ProductDesigner from "./Staff/productDesigner.svg";
+import ProjectManager from "./Staff/projectManager.svg";
+import QaEngineer from "./Staff/qaEngineer.svg";
+import SolutionArchitect from "./Staff/solutionArchitect.svg";
+import TeachLead from "./Staff/teachLead.svg";
+import Ui from "./Staff/ui.svg";
+import Wordpress from "./Staff/wordpress.svg";
+import Appdev from "./Staff/phone.svg";
 
-
-
+// Export assets
 export const assets = {
     logo,
     product,
@@ -62,7 +64,7 @@ export const assets = {
     case1,
     case2,
     case3,
-    letsdiscuss,
+    letsDiscuss,
     formimg,
     buttonArrow,
     contactImage,
@@ -99,11 +101,9 @@ export const staff = {
     environment,
     flexible,
     recognition
-
 };
 
 // techStack.js
-
 import js from './techStack/js.svg';
 import react from './techStack/react.svg';
 import vue from './techStack/vue.svg';
@@ -114,27 +114,27 @@ import node from './techStack/node.svg';
 import java from './techStack/java.svg';
 import python from './techStack/python.svg';
 import php from './techStack/php.svg';
-import csharp from './techStack/csharp.svg'
-import cpp from './techStack/cpp.svg'
-import laravel from './techStack/laravel.svg'
-import symfony from './techStack/symfony.svg'
-import django from './techStack/django.svg'
-import express from './techStack/express.svg'
-import spring from './techStack/spring.svg'
-import ios from './techStack/ios.svg'
-import native from './techStack/native.svg'
-import flutter from './techStack/flutter.svg'
-import kotlin from './techStack/kotlin.svg'
-import ionic from './techStack/ionic.svg'
-import mysql from './techStack/mysql.svg'
-import sqlserver from './techStack/sqlserver.svg'
-import postgre from './techStack/postgre.svg'
-import mariadb from './techStack/mariadb.svg'
-import redis from './techStack/redis.svg'
-import oracle from './techStack/oracle.svg'
-import go from './techStack/go.svg'
+import csharp from './techStack/csharp.svg';
+import cpp from './techStack/cpp.svg';
+import laravel from './techStack/laravel.svg';
+import symfony from './techStack/symfony.svg';
+import django from './techStack/django.svg';
+import express from './techStack/express.svg';
+import spring from './techStack/spring.svg';
+import ios from './techStack/ios.svg';
+import native from './techStack/native.svg';
+import flutter from './techStack/flutter.svg';
+import kotlin from './techStack/kotlin.svg';
+import ionic from './techStack/ionic.svg';
+import mysql from './techStack/mysql.svg';
+import sqlserver from './techStack/sqlserver.svg';
+import postgre from './techStack/postgre.svg';
+import mariadb from './techStack/mariadb.svg';
+import redis from './techStack/redis.svg';
+import oracle from './techStack/oracle.svg';
+import go from './techStack/go.svg';
 
-export const techStack ={
+export const techStack = {
     js,
     react,
     vue,
@@ -174,5 +174,4 @@ export const company = [
     "/logos/logo4.png",
     "/logos/logo5.png",
     "/logos/logo6.png"
-  ];
-  
+];
